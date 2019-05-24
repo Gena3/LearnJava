c80cff759f8e807132df57e3f8d2551dc0e877a6
Gena3 <gena.zyzykin@yandex.ru>
2019-05-24 20:33:12 +0300

    Выполнил вторую задачку из раздела про исключния.

--- testExceptoin/src/MainClass.java
+++ testExceptoin/src/MainClass.java
@@ -5,8 +5,19 @@ public class MainClass {
         String retS = "Except";
         return retS;
     }
-
+    private static double sqrt(double x) {
+        if (x < 0){
+            throw new IllegalArgumentException("Expected non-negative number, got " + x);
+        }
+        return Math.sqrt(x); // your implementation here
+    }
     public static void main(String[] args) {
-        Double b = (Double) MainClass.retObject();
+        // Double b = (Double) MainClass.retObject();
+        Double b = 10.0;
+        for(int i = 0; i < 2; i++){
+            System.out.println("Arg = " + b + "; result = " + sqrt(b));
+            b -= 15.0;
+        }
+
     }
 }
