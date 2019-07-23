dddb533227f6dcdd9c34f4dbb222c40485f0943e
Gena3 <gena.zyzykin@yandex.ru>
2019-07-23 17:54:47 +0300

    - Complete step 7 from Section 4.2

--- testExceptoin/src/MainClass.java
+++ testExceptoin/src/MainClass.java
@@ -1,4 +1,4 @@
-
+import java.io.IOException;
 
 public class MainClass {
     private static Object retObject(){
@@ -11,13 +11,36 @@ public class MainClass {
         }
         return Math.sqrt(x); // your implementation here
     }
+
+    public static String getCallerClassAndMethodName(){
+        String strReturn;
+        StackTraceElement elStackTrArr[];
+        try{
+            throw new IOException();
+        }catch(IOException e){
+            elStackTrArr = e.getStackTrace();
+            strReturn = elStackTrArr[1].getClassName();
+            if(strReturn != null)
+            {
+                strReturn = strReturn + "#" + elStackTrArr[1].getMethodName();
+            }
+        }
+        return strReturn;
+    }
+    //
+    private static void anotherMethod(){
+        System.out.println(getCallerClassAndMethodName());
+    }
+
     public static void main(String[] args) {
         // Double b = (Double) MainClass.retObject();
-        Double b = 10.0;
-        for(int i = 0; i < 2; i++){
-            System.out.println("Arg = " + b + "; result = " + sqrt(b));
-            b -= 15.0;
-        }
+        // Double b = 10.0;
+        // for(int i = 0; i < 2; i++){
+            // System.out.println("Arg = " + b + "; result = " + sqrt(b));
+            // b -= 15.0;
+        //}
+        System.out.println(getCallerClassAndMethodName());
+        anotherMethod();
 
     }
 }
